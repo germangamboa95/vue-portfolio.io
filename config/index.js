19cd8c2a01a74ed5a68166ca5369f51655c230d5
German Gamboa <germangamboa95@gmail.com>
2018-06-25 15:49:22 -0400

    paths where messed up

--- config/index.js
+++ config/index.js
@@ -50,7 +50,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: './vue-portfolio.io',
+    assetsPublicPath: './',
 
     /**
      * Source Maps
